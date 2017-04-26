5bff15dc6fb7edf110748ebc08ebdb93272b3f2b
mynewyear <yanata.4a@gmail.com>
2017-04-25 22:58:19 -0700

    IDEA Project was created

--- sandbox/src/main/java/MyFirstProgramm.java
+++ sandbox/src/main/java/MyFirstProgramm.java
@@ -1,5 +1,5 @@
-public class MyFirstProgramm{
-	public static void main(String[] args){
-		System.out.println("Hello World!");
-	}
+public class MyFirstProgramm {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
